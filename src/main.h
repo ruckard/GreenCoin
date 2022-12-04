e5f6d11610d682c5e744a54112bcbf2f5d5e2754
Ruckard Smith <ruckard.smith@gmail.com>
2022-12-04 19:29:45 +0100

    FORK-GHOST: [CHANGE-GENESIS]

--- src/main.h
+++ src/main.h
@@ -49,8 +49,8 @@ static const int fHaveUPnP = true;
 static const int fHaveUPnP = false;
 #endif
 
-static const uint256 hashGenesisBlockOfficial("0xaf4ac34e7ef10a08fe2ba692eb9a9c08cf7e89fcf352f9ea6f0fd73ba3e5d03c");
-static const uint256 hashGenesisBlockTestNet ("0xaf4ac34e7ef10a08fe2ba692eb9a9c08cf7e89fcf352f9ea6f0fd73ba3e5d03c");
+static const uint256 hashGenesisBlockOfficial("0x00000d281cb2873658667c29e5e28079ead13604d022e779facc7b2f4bd75c60");
+static const uint256 hashGenesisBlockTestNet ("0x");
 
 static const int64 nMaxClockDrift = 2 * 60 * 60;        // two hours
 
